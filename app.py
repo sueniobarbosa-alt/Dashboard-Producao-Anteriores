859a138bf8ab90b1165098d1b01f48b985e131f6
sueniobarbosa-alt <suenio.barbosa@stellantis.com>
2026-06-24 16:55:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -57,15 +57,70 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.markdown("""
-    <div class="card">
-        <div class="verde">IMPOSTADA 120</div>
-        <br>
-        <div class="producao">551</div>
-        <br>
-        <div class="vermelho">DELTA -38</div>
-        <br>
-        <div class="amarelo">GIRO LF: 0</div>
-        <div class="amarelo">RETRABALHO: 0</div>
+    <div style="border:1px solid white; background:black; color:white;">
+
+        <table style="width:100%; border-collapse:collapse; text-align:center;">
+            <tr>
+                <td style="border:1px solid white;">
+                    Impostada<br>
+                    <span style="color:#00ff00;font-size:40px;"><b>120</b></span>
+                </td>
+
+                <td style="border:1px solid white;">
+                    Teórica<br>
+                    <span style="color:#00ff00;font-size:40px;"><b>120</b></span>
+                </td>
+
+                <td style="border:1px solid white;">
+                    <span style="font-size:70px;"><b>551</b></span><br>
+                    <span style="color:#00ff00;font-size:30px;">
+                    Produção Anteriores
+                    </span>
+                </td>
+
+                <td style="border:1px solid white;">
+                    Parcial<br>
+                    <span style="color:#00ff00;font-size:40px;"><b>82</b></span>
+                </td>
+
+                <td style="border:1px solid white;">
+                    Delta<br>
+                    <span style="color:red;font-size:40px;"><b>-38</b></span>
+                </td>
+            </tr>
+
+            <tr>
+                <td colspan="2" style="border:1px solid white;">
+                    Giro Linha Final<br>
+                    <span style="color:yellow;font-size:35px;">0</span>
+                </td>
+
+                <td style="border:1px solid white;">
+                    STELLANTIS
+                </td>
+
+                <td colspan="2" style="border:1px solid white;">
+                    Giro Retrabalho<br>
+                    <span style="color:yellow;font-size:35px;">0</span>
+                </td>
+            </tr>
+
+            <tr>
+                <td colspan="5" style="padding:20px;">
+                    CONTABILIZANDO GIRO LINHA FINAL
+                    <span style="color:red;font-size:35px;"><b>-38</b></span>
+                </td>
+            </tr>
+
+            <tr>
+                <td colspan="5" style="padding:20px;">
+                    CONTABILIZANDO GIRO LINHA FINAL E RETRABALHO
+                    <span style="color:red;font-size:35px;"><b>-38</b></span>
+                </td>
+            </tr>
+
+        </table>
+
     </div>
     """, unsafe_allow_html=True)
 

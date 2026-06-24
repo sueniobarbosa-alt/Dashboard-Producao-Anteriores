86dc2737d1e18e5f69c5c1e88b65bc1c04ff1c97
sueniobarbosa-alt <suenio.barbosa@stellantis.com>
2026-06-24 17:06:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,61 +7,15 @@ st.set_page_config(
 
 st.markdown("""
 <style>
-.stApp{
-    background-color:black;
-    color:white;
+.stApp {
+    background-color: black;
 }
 
-table{
-    width:100%;
-    border-collapse:collapse;
-    text-align:center;
-    background:black;
-    color:white;
-}
-
-td{
-    border:1px solid white;
-    padding:8px;
-}
-
-.valor-verde{
-    color:#00ff00;
-    font-size:40px;
-    font-weight:bold;
-}
-
-.valor-vermelho{
-    color:red;
-    font-size:40px;
-    font-weight:bold;
-}
-
-.producao{
-    font-size:70px;
-    font-weight:bold;
-}
-
-.titulo{
-    color:#00ff00;
-    font-size:28px;
-    font-weight:bold;
-}
-
-.amarelo{
-    color:yellow;
-    font-size:35px;
-    font-weight:bold;
-}
-
-.hora{
-    font-size:30px;
-    font-weight:bold;
-}
-
-.data{
-    font-size:24px;
-    font-weight:bold;
+div[data-testid="stMetric"] {
+    background-color: black;
+    border: 1px solid white;
+    padding: 15px;
+    border-radius: 5px;
 }
 </style>
 """, unsafe_allow_html=True)
@@ -69,156 +23,46 @@ td{
 st.title("STELLANTIS - JABOATÃO")
 st.subheader("REPORT PRODUÇÃO ANTERIORES")
 
+def card(impostada, teorica, producao, parcial,
+         delta, giro, retrabalho):
+
+    st.write(f"**Impostada:** {impostada}")
+    st.write(f"**Teórica:** {teorica}")
+
+    st.metric(
+        label="Produção Anteriores",
+        value=producao,
+        delta=delta
+    )
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.metric(
+            "Giro Linha Final",
+            giro
+        )
+
+    with col2:
+        st.metric(
+            "Retrabalho",
+            retrabalho
+        )
+
+    st.divider()
+
 col1, col2 = st.columns(2)
 
 with col1:
-    st.markdown("""
-    <table>
-        <tr>
-            <td>
-                Impostada<br>
-                <div class="valor-verde">120</div>
-            </td>
-
-            <td>
-                Teórica<br>
-                <div class="valor-verde">120</div>
-            </td>
-
-            <td>
-                <div class="producao">551</div>
-                <div class="titulo">Produção Anteriores</div>
-            </td>
-
-            <td>
-                Parcial<br>
-                <div class="valor-verde">82</div>
-            </td>
-
-            <td>
-                Delta<br>
-                <div class="valor-vermelho">-38</div>
-            </td>
-        </tr>
-
-        <tr>
-            <td colspan="2">
-                Giro linha Final<br>
-                <div class="amarelo">0</div>
-            </td>
-
-            <td>
-                STELLANTIS
-            </td>
-
-            <td colspan="2">
-                Giro Retrabalho<br>
-                <div class="amarelo">0</div>
-            </td>
-        </tr>
-
-        <tr>
-            <td colspan="2">
-                <div class="hora">16:21:04</div>
-            </td>
-
-            <td>
-                STELLANTIS
-            </td>
-
-            <td colspan="2">
-                <div class="data">24/06/2026</div>
-            </td>
-        </tr>
-
-        <tr>
-            <td colspan="5">
-                CONTABILIZANDO GIRO LINHA FINAL
-                <span class="valor-vermelho">-38</span>
-            </td>
-        </tr>
-
-        <tr>
-            <td colspan="5">
-                CONTABILIZANDO GIRO LINHA FINAL E RETRABALHO
-                <span class="valor-vermelho">-38</span>
-            </td>
-        </tr>
-    </table>
-    """, unsafe_allow_html=True)
+    card(120,120,551,-38,-38,0,0)
 
 with col2:
-    st.markdown("""
-    <table>
-        <tr>
-            <td>
-                Impostada<br>
-                <div class="valor-verde">204</div>
-            </td>
+    card(204,204,226,-64,-64,0,0)
 
-            <td>
-                Teórica<br>
-                <div class="valor-verde">204</div>
-            </td>
+col3, col4 = st.columns(2)
 
-            <td>
-                <div class="producao">226</div>
-                <div class="titulo">Produção Anteriores</div>
-            </td>
+with col3:
+    card(168,168,521,-42,-42,0,0)
 
-            <td>
-                Parcial<br>
-                <div class="valor-verde">140</div>
-            </td>
-
-            <td>
-                Delta<br>
-                <div class="valor-vermelho">-64</div>
-            </td>
-        </tr>
-
-        <tr>
-            <td colspan="2">
-                Giro linha Final<br>
-                <div class="amarelo">0</div>
-            </td>
-
-            <td>
-                STELLANTIS
-            </td>
-
-            <td colspan="2">
-                Giro Retrabalho<br>
-                <div class="amarelo">0</div>
-            </td>
-        </tr>
-
-        <tr>
-            <td colspan="2">
-                <div class="hora">16:21:04</div>
-            </td>
-
-            <td>
-                STELLANTIS
-            </td>
-
-            <td colspan="2">
-                <div class="data">24/06/2026</div>
-            </td>
-        </tr>
-
-        <tr>
-            <td colspan="5">
-                CONTABILIZANDO GIRO LINHA FINAL
-                <span class="valor-vermelho">-64</span>
-            </td>
-        </tr>
-
-        <tr>
-            <td colspan="5">
-                CONTABILIZANDO GIRO LINHA FINAL E RETRABALHO
-                <span class="valor-vermelho">-64</span>
-            </td>
-        </tr>
-    </table>
-    """, unsafe_allow_html=True)
+with col4:
+    card(260,260,363,-99,-99,0,0)

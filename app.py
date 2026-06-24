5064fad3ca05d20f1255c9739d2c42182ef3e6bb
sueniobarbosa-alt <suenio.barbosa@stellantis.com>
2026-06-24 16:44:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,76 +1,84 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Produção Anteriores",
+    layout="wide"
+)
+
 st.markdown("""
 <style>
-.card{
-    border:1px solid white;
-    background:black;
-    color:white;
-    margin:10px;
+.stApp {
+    background-color: black;
+    color: white;
 }
 
-.topo{
-    display:grid;
-    grid-template-columns:1fr 1fr 2fr 1fr 1fr;
-    border-bottom:1px solid white;
+.card {
+    border: 1px solid white;
+    padding: 20px;
+    text-align: center;
+    background: black;
 }
 
-.bloco{
-    text-align:center;
-    padding:5px;
-    border-right:1px solid white;
+.verde {
+    color: #00ff00;
+    font-size: 30px;
+    font-weight: bold;
 }
 
-.valor-verde{
-    color:#00ff00;
-    font-size:28px;
-    font-weight:bold;
+.amarelo {
+    color: yellow;
+    font-size: 28px;
+    font-weight: bold;
 }
 
-.valor-amarelo{
-    color:yellow;
-    font-size:28px;
-    font-weight:bold;
+.vermelho {
+    color: red;
+    font-size: 28px;
+    font-weight: bold;
 }
 
-.valor-vermelho{
-    color:red;
-    font-size:28px;
-    font-weight:bold;
+.producao {
+    font-size: 70px;
+    font-weight: bold;
 }
 
-.producao{
-    font-size:60px;
-    font-weight:bold;
-}
-
-.titulo{
-    color:#00ff00;
-    font-size:28px;
-    font-weight:bold;
-}
-
-.meio{
-    display:grid;
-    grid-template-columns:1fr 2fr 1fr;
-    border-bottom:1px solid white;
-}
-
-.rodape{
-    padding:10px;
-    text-align:center;
-    font-size:26px;
-}
-
-.data{
-    font-size:18px;
-    font-weight:bold;
-}
-
-.logo{
-    text-align:center;
-    padding-top:10px;
+.titulo {
+    color: #00ff00;
+    font-size: 28px;
+    font-weight: bold;
 }
 </style>
 """, unsafe_allow_html=True)
 
+st.title("STELLANTIS - JABOATÃO")
+st.subheader("REPORT PRODUÇÃO ANTERIORES")
+
 col1, col2 = st.columns(2)
 
+with col1:
+    st.markdown("""
+    <div class="card">
+        <div class="verde">IMPOSTADA 120</div>
+        <br>
+        <div class="producao">551</div>
+        <br>
+        <div class="vermelho">DELTA -38</div>
+        <br>
+        <div class="amarelo">GIRO LF: 0</div>
+        <div class="amarelo">RETRABALHO: 0</div>
+    </div>
+    """, unsafe_allow_html=True)
+
+with col2:
+    st.markdown("""
+    <div class="card">
+        <div class="verde">IMPOSTADA 204</div>
+        <br>
+        <div class="producao">226</div>
+        <br>
+        <div class="vermelho">DELTA -64</div>
+        <br>
+        <div class="amarelo">GIRO LF: 0</div>
+        <div class="amarelo">RETRABALHO: 0</div>
+    </div>
+    """, unsafe_allow_html=True)

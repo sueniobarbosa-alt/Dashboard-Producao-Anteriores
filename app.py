f637de54022de60754ef5bbeeb57821896e22530
sueniobarbosa-alt <suenio.barbosa@stellantis.com>
2026-06-24 16:24:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,112 +1,247 @@
 import streamlit as st
-from datetime import datetime
+
 st.set_page_config(
-    page_title="Produção Jaboatão",
+    page_title="Produção Anteriores",
     layout="wide"
 )
-st.image("file.png", width=250)
 
-st.markdown(
-    f"<h3 style='color:white'>Atualização: {datetime.now().strftime('%H:%M:%S')}</h3>",
-    unsafe_allow_html=True
-)
 st.markdown("""
 <style>
+
 .stApp{
-    background-color:black;
+    background-color:#000000;
+    color:white;
 }
 
 .card{
     border:1px solid white;
-    padding:10px;
-    height: 280px;
-    margin:10px;
-    background-color:black;
-    color:white;
-    text-align:center;
+    background:black;
+    padding:0;
+    margin-bottom:20px;
 }
 
-.valor{
+.linha{
+    display:flex;
+}
+
+.bloco{
+    flex:1;
+    text-align:center;
+    border-right:1px solid white;
+    padding:5px;
+}
+
+.bloco:last-child{
+    border-right:none;
+}
+
+.valor-verde{
+    color:#00ff00;
+    font-size:28px;
+    font-weight:bold;
+}
+
+.valor-amarelo{
+    color:yellow;
+    font-size:28px;
+    font-weight:bold;
+}
+
+.valor-vermelho{
+    color:red;
+    font-size:28px;
+    font-weight:bold;
+}
+
+.producao{
     font-size:55px;
     font-weight:bold;
     color:white;
 }
 
-.verde{
+.titulo{
     color:#00ff00;
-    font-size:30px;
+    font-size:28px;
+    font-weight:bold;
 }
 
-.vermelho{
+.hora{
+    font-size:24px;
+    font-weight:bold;
+}
+
+.rodape{
+    text-align:center;
+    font-size:18px;
+    padding:8px;
+}
+
+.delta{
     color:red;
-    font-size:30px;
+    font-size:24px;
+    font-weight:bold;
 }
 
-.amarelo{
-    color:yellow;
-    font-size:25px;
-}
 </style>
 """, unsafe_allow_html=True)
 
-st.markdown(
-    "<h1 style='color:white'>STELLANTIS - JABOATÃO</h1>",
-    unsafe_allow_html=True
-)
-
-st.markdown(
-    "<h2 style='color:white'>REPORT PRODUÇÃO ANTERIORES</h2>",
-    unsafe_allow_html=True
-)
+st.title("STELLANTIS - JABOATÃO")
+st.subheader("REPORT PRODUÇÃO ANTERIORES")
 
 col1, col2 = st.columns(2)
 
 with col1:
-   st.markdown("""
-    <div class='card'>
-    <h2>JEEP</h2>
-    <div class='valor'>551</div>
-    <div class='vermelho'>Δ -9</div>
-    <br>
-    <div class='amarelo'>GIRO LF: 6</div>
-    <div class='amarelo'>RETRABALHO: 1</div>
+
+    st.markdown(f"""
+    <div class="card">
+
+        <div class="linha">
+            <div class="bloco">
+                Impostada<br>
+                <div class="valor-verde">120</div>
+            </div>
+
+            <div class="bloco">
+                Teórica<br>
+                <div class="valor-verde">120</div>
+            </div>
+
+            <div class="bloco">
+                <div class="producao">551</div>
+                <div class="titulo">Produção Anteriores</div>
+            </div>
+
+            <div class="bloco">
+                Parcial<br>
+                <div class="valor-verde">82</div>
+            </div>
+
+            <div class="bloco">
+                Delta<br>
+                <div class="valor-vermelho">-38</div>
+            </div>
+        </div>
+
+        <div class="linha">
+            <div class="bloco">
+                Giro Linha Final<br>
+                <div class="valor-amarelo">0</div>
+            </div>
+
+            <div class="bloco">
+                <br>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Stellantis.svg/512px-Stellantis.svg.png" width="220">
+            </div>
+
+            <div class="bloco">
+                Giro Retrabalho<br>
+                <div class="valor-amarelo">0</div>
+            </div>
+        </div>
+
+        <div class="linha">
+            <div class="bloco">
+                <div class="hora">16:21:04</div>
+            </div>
+
+            <div class="bloco">
+            </div>
+
+            <div class="bloco">
+                <div class="hora">
+                24/06/2026<br>
+                Wednesday
+                </div>
+            </div>
+        </div>
+
+        <div class="rodape">
+            CONTABILIZANDO GIRO LINHA FINAL
+            <span class="delta"> -38</span>
+        </div>
+
+        <div class="rodape">
+            CONTABILIZANDO GIRO LINHA FINAL E RETRABALHO
+            <span class="delta"> -38</span>
+        </div>
+
     </div>
     """, unsafe_allow_html=True)
-    
+
 with col2:
-    st.markdown("""
-    <div class='card'>
-    <h2>JEEP</h2>
-    <div class='valor'>226</div>
-    <div class='vermelho'>Δ -26</div>
-    <br>
-    <div class='amarelo'>GIRO LF: 23</div>
-    <div class='amarelo'>RETRABALHO: 7</div>
-    </div>
-    """, unsafe_allow_html=True)
 
-col3, col4 = st.columns(2)
+    st.markdown(f"""
+    <div class="card">
 
-with col3:
-    st.markdown("""
-    <div class='card'>
-    <h2>JEEP</h2>
-    <div class='valor'>521</div>
-    <div class='vermelho'>Δ -11</div>
-    <br>
-    <div class='amarelo'>GIRO LF: 10</div>
-    <div class='amarelo'>RETRABALHO: 3</div>
-    </div>
-    """, unsafe_allow_html=True)
+        <div class="linha">
+            <div class="bloco">
+                Impostada<br>
+                <div class="valor-verde">204</div>
+            </div>
 
-with col4:
-    st.markdown("""
-    <div class='card'>
-    <h2>FIAT</h2>
-    <div class='valor'>363</div>
-    <div class='vermelho'>Δ -10</div>
-    <br>
-    <div class='amarelo'>GIRO LF: 13</div>
-    <div class='amarelo'>RETRABALHO: 5</div>
+            <div class="bloco">
+                Teórica<br>
+                <div class="valor-verde">204</div>
+            </div>
+
+            <div class="bloco">
+                <div class="producao">226</div>
+                <div class="titulo">Produção Anteriores</div>
+            </div>
+
+            <div class="bloco">
+                Parcial<br>
+                <div class="valor-verde">140</div>
+            </div>
+
+            <div class="bloco">
+                Delta<br>
+                <div class="valor-vermelho">-64</div>
+            </div>
+        </div>
+
+        <div class="linha">
+            <div class="bloco">
+                Giro Linha Final<br>
+                <div class="valor-amarelo">0</div>
+            </div>
+
+            <div class="bloco">
+                <br>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/Stellantis.svg/512px-Stellantis.svg.png" width="220">
+            </div>
+
+            <div class="bloco">
+                Giro Retrabalho<br>
+                <div class="valor-amarelo">0</div>
+            </div>
+        </div>
+
+        <div class="linha">
+            <div class="bloco">
+                <div class="hora">16:21:04</div>
+            </div>
+
+            <div class="bloco">
+            </div>
+
+            <div class="bloco">
+                <div class="hora">
+                24/06/2026<br>
+                Wednesday
+                </div>
+            </div>
+        </div>
+
+        <div class="rodape">
+            CONTABILIZANDO GIRO LINHA FINAL
+            <span class="delta"> -64</span>
+        </div>
+
+        <div class="rodape">
+            CONTABILIZANDO GIRO LINHA FINAL E RETRABALHO
+            <span class="delta"> -64</span>
+        </div>
+
     </div>
     """, unsafe_allow_html=True)
